72a821e844be85b5b09e663e3f213fffcad9afde
1evanter <124162782+1evanter@users.noreply.github.com>
2023-07-04 11:00:24 +0000

    Deploying to gh-pages from @ 1evanter/goit-js-hw-09@7a0cbbcd833c0d5b7d3ff3436dbd8215a5951a43 🚀

---
+++ 01-color-switcher.1e478fe2.js
@@ -0,0 +1,2 @@
+!function(){let t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null,o=!1;t.addEventListener("click",function(){o||(o=!0,n=setInterval(()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`},1e3))}),e.addEventListener("click",function(){clearInterval(n),o=!1})}();
+//# sourceMappingURL=01-color-switcher.1e478fe2.js.map

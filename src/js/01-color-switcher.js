7a0cbbcd833c0d5b7d3ff3436dbd8215a5951a43
1evanter <rita.oleksenkoo@gmail.com>
2023-07-04 13:00:00 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+let intervalId = null;
+let isActive = false;
+
+startBtnEl.addEventListener('click', onStartBtn);
+stopBtnEl.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+    if (isActive) {
+        return;
+    };
+    
+    isActive = true;
+   intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+};
+
+function onStopBtn() {
+    clearInterval(intervalId);
+    isActive = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
 

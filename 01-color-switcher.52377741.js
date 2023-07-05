7eddbb943704f2b9e5e24d18eebd08e21eaede54
1evanter <124162782+1evanter@users.noreply.github.com>
2023-07-05 12:26:19 +0000

    Deploying to gh-pages from @ 1evanter/goit-js-hw-09@1682ff6631e92e9898e58feda0d1b500a60e9c69 🚀

--- 01-color-switcher.52377741.js
+++
@@ -1,2 +0,0 @@
-const t={startBtnEl:document.querySelector("button[data-start]"),stopBtnEl:document.querySelector("button[data-stop]")};let n=null,e=!1;t.startBtnEl.addEventListener("click",function(){e||(e=!0,n=setInterval(()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`},1e3))}),t.stopBtnEl.addEventListener("click",function(){clearInterval(n),e=!1});
-//# sourceMappingURL=01-color-switcher.52377741.js.map
--- 01-color-switcher.d2eb50a4.js
+++
@@ -1,2 +0,0 @@
-!function(){let t={startBtnEl:document.querySelector("button[data-start]"),stopBtnEl:document.querySelector("button[data-stop]")},n=null,e=!1;t.startBtnEl.addEventListener("click",function(){e||(e=!0,n=setInterval(()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`},1e3))}),t.stopBtnEl.addEventListener("click",function(){clearInterval(n),e=!1})}();
-//# sourceMappingURL=01-color-switcher.d2eb50a4.js.map
